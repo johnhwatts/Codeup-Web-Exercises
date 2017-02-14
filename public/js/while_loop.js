eb11dcc3945a48ac154e024d20882da9c44d04a7
John Watts <johnwatts@gmx.com>
2017-02-14 15:11:49 -0600

    While Loop updates after walk through

--- public/js/while_loop.js
+++ public/js/while_loop.js
@@ -3,18 +3,19 @@
 //Question 2:
 
  // This is how you get a random number between 50 and 100
- var allCones = Math.floor(Math.random() * 50) + 50;
- // This is how you get a random number between 1 and 5
- var cones = Math.floor(Math.random() * 5) + 1;
- var i = 5;
- do {
-	console.log(cones + " cones sold");
-	i++;
- if (allCones > cones) {
-	allCones = (allCones - cones);
- }
- } while (100 >= allCones > 0);
+ var coneInventory = Math.floor(Math.random() * 50) + 50;
+ 
+ do { 
+ 	var conesToSell = Math.floor(Math.random() * 5) + 1;
+ 	// if we have that many left, subtract it from our inventory
+	if (conesToSell <= coneInventory) {
+ 	console.log("Sold" + conesToSell + " cones...")
+ 	coneInventory = coneInventory - conesToSell;
+ 	} else {
+ 		console.log("I don't have enough!")
+ 	}
+} while (coneInventory > 0);
 
- console.log(allCones);
+ console.log("Sold all the cones!");
 
 
