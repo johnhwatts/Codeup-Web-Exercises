c38a5aec49b522429b12009de9bf2da63409a638
John Watts <johnwatts@gmx.com>
2017-02-16 16:46:28 -0600

    More Function exercises

--- public/js/functions_extras.js
+++ public/js/functions_extras.js
@@ -1,87 +1,91 @@
 "use strict";
 
-//Exercise 
+// //Exercise 
 
-function log(input) {
-	console.log(input);
-}
+// function log(input) {
+// 	console.log(input);
+// }
 
-var result = log("Yes");
-console.log(result);      
+// result = log("Yes");
+// console.log(result);      
 
-//Exercise 2
+// //Exercise 2
 
-function identity(argument) {
-	console.log(argument);
-}
+// function identity(argument) {
+// 	return argument;
+// }
 
-var result = identity("This is tough!");
-console.log(result);
+// result = identity("This is tough!");
+// console.log(result);
 
-//Exercise 3
+// //Exercise 3
 
-function getRandomIntBetween(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (11 - 1)) + 1;
-}
+// function getRandomIntBetween(min, max) {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+//   return Math.floor(Math.random() * (max - min)) + min;
+// }
 
-var result = getRandomIntBetween();
-	console.log(result);
+// 	var random = getRandomIntBetween(1, 10);
+// 	console.log(random);
 
 //Exercise 4
 
 function isNumeric(input) {
-		var result;
-
-		if(input  === 0 || input === "") {
-			result = true;
-		} else {
-			result = false;
-		}
-		return result;
+		//how to check if a value is numeric in JS
+		return !isNaN(input);
 }
-var result = isNumeric()
-console.log(result);
 
 // Exercise 5
 
 //add
 function add(a, b) {
-    return a + b;
+	if(isNumeric(a) && isNumeric(b)) {
+		return a + b;
+	}
 }
-var result = add(2,4);
-console.log(result);
+// var result = add(2,4);
+// console.log(result);
 
 //subtract
 function subtract(a, b) {
     return a - b;
 }
-var result = subtract(2,4);
-console.log(result);
+// var result = subtract(2,4);
+// console.log(result);
 
 //multiply
 function multiply(a, b) {
     return a * b;
 }
-var result = multiply(2,4);
-console.log(result);
+// var result = multiply(2,4);
+// console.log(result);
 
 //divide
 function divide(a, b) {
     return a / b;
 }
-var result = divide(2,4);
-console.log(result);
+// var result = divide(2,4);
+// console.log(result);
 
-// Exercise 6
+// // Exercise 6
 
 function square(a) {
-	return multiply;
+	return multiply(a, a);
 }
-var result = square(4);
-console.log(result);
+// var result = square(4);
+// console.log(result);
 
+//Bonus:
+
+fucntion sumOfSquares(a, b) {
+	//all these do the same thing
+	return a*a + b*b;
+	return square(a) + square(b);
+	return sum(square(a), square(b));
+	return sum(multiply(a, a), multiply(b, b));
+}
+console.log(sumOfSquares(2, 3));
 
 
 
