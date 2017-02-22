1928225f09b9a95721d37f6f4054445d4a41f51c
John Watts <johnwatts@gmx.com>
2017-02-22 10:28:06 -0600

    JS Calculator with parseFloat

--- public/js/js_calculator.js
+++ public/js/js_calculator.js
@@ -1,25 +1,23 @@
+(function () {
+
 "use strict";
 		
-		var leftDisplay = document.getElementById("leftDisplay");
-		var centerDisplay = document.getElementById("centerDisplay");
-		var rightDisplay = document.getElementById("rightDisplay");
-
-		var numbers = document.getElementsByClassName("numbers");
-
-		function displayNumber() {
-			// append the display's inner text with the innerText of the button clicked
-			
-			if (centerDisplay.innerText == "" && leftDisplay.innerText == "") {
-				leftDisplay.innerText += this.innerText;
-			} else {
-				rightDisplay.innerText += this.innerText;
-			}
+	var leftDisplay = document.getElementById("leftDisplay");
+	var centerDisplay = document.getElementById("centerDisplay");
+	var rightDisplay = document.getElementById("rightDisplay");
+	var numbers = document.getElementsByClassName("numbers");
+	function displayNumber() {
+		// append the display's inner text with the innerText of the button clicked
+		
+		if (centerDisplay.innerText == "") {
+			leftDisplay.innerText += this.innerText;
+		} else {
+			rightDisplay.innerText += this.innerText;
 		}
-
-		for(var i = 0; i < numbers.length; i++) {
-			numbers[i].addEventListener("click", displayNumber);
-
-		}
+	}
+	for(var i = 0; i < numbers.length; i++) {
+		numbers[i].addEventListener("click", displayNumber);
+	}
 
  
 function math() {
@@ -27,29 +25,37 @@ function math() {
 	var result;
 	switch (centerDisplay.innerText) {
 		case "+": 
-			result = parseInt(leftDisplay.innerText) + parseInt(rightDisplay.innerText);
+			result = parseFloat(leftDisplay.innerText) + parseFloat(rightDisplay.innerText);
 			break;
 		case "-":
-			result = parseInt(leftDisplay.innerText) - parseInt(rightDisplay.innerText);
+			result = parseFloat(leftDisplay.innerText) - parseFloat(rightDisplay.innerText);
 			break;
 		case "*":
-			result = parseInt(leftDisplay.innerText) * parseInt(rightDisplay.innerText);
+			result = parseFloat(leftDisplay.innerText) * parseFloat(rightDisplay.innerText);
 			break;
 		case "/":
-			result = parseInt(leftDisplay.innerText) / parseInt(rightDisplay.innerText);
+			result = parseFloat(leftDisplay.innerText) / parseFloat(rightDisplay.innerText);
 			break;
-	}
-}
-		document.getElementById("equals").addEventListener("click", math);	
 
-function clear () {
+	}
+	clear();
+	leftDisplay.innerText = result;
+}
+
+
+		
+document.getElementById("equals").addEventListener("click", math);	
+
+function clear() {
 	
 	centerDisplay.innerText = "";
 	leftDisplay.innerText = "";
 	rightDisplay.innerText = "";
 
-	leftDisplay.innerText = result;
+	
 }
+
+
 
 document.getElementById("clear").addEventListener("click", clear);
 
@@ -68,3 +74,5 @@ document.getElementById("multiply").addEventListener("click", function() {
 document.getElementById("divide").addEventListener("click", function() {
 	centerDisplay.innerText = "/";
 })
+
+})();
