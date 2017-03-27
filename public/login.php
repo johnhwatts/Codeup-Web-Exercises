412645dfc0e36e6901c792b8764a0e2fcadbcde7
John Watts <johnwatts@gmx.com>
2017-03-27 16:58:28 -0500

    Updated login exercise to include comments and empty condition

--- public/login.php
+++ public/login.php
@@ -8,7 +8,8 @@ function pageController() {
     $username = isset($_POST['username']) ? $_POST['username'] : '';
     $password = isset($_POST['password']) ? $_POST['password'] : '';
     //if a value exists, get that value and assign to variable, else assign default value
-    if (! empty($_POST['username']) && ! empty($_POST['password'])) {
+    //use !empty so that you don't get the "login failed" message on page load.
+    if (!empty($_POST['username']) && !empty($_POST['password'])) {
         if ($username == 'guest' && $password == 'password') {
         header("Location: http://codeup.dev/authorized.php");
         } else {
@@ -33,11 +34,15 @@ extract(pageController());
 	<link rel="stylesheet" type="text/css" href="/css/counter.css"> 
     </head>
     <body>
-        <form name="login" method="POST">
-            Username<input type="text" name="username"/>
-            Password<input type="password" name="password"/>
-            <input type="submit"/>
-        </form>
-    	
+        <main class="container">
+            <form name="login" method="POST">
+                <div class="form-group">
+                Username: <input type="text" name="username" placeholder="username" /></div>
+                <div class="form-group">
+                Password: <input type="password" name="password" placeholder="password" /></div>
+                <div class="form-group">
+                <input type="submit"/></div>
+            </form>
+    	</main>
     </body>
 </html>
