d93c98b2e8e8185475bf9e436fbd2367113ac974
John Watts <johnwatts@gmx.com>
2017-03-27 10:55:23 -0500

    Counter complete and working

--- public/php/counter.php
+++ public/php/counter.php
@@ -8,29 +8,40 @@
  */
 function pageController()
 {
-    // Initialize an empty data array.
-    $data = array();
-
-    // Add data to be used in the html view.
-    $data['message'] = 'Hello Codeup!';
-
-    // Return the completed data array.
-    return $data;
+    if (! isset($_GET['counter'])) {
+		$_GET['counter'] = 0;
+	}
+	return $_GET;
 }
 
 // Call the pageController function and extract all the returned array as local variables.
 extract(pageController());
 
-// Only use echo, conditionals, and loops anywhere within the HTML.
 ?>
 
 
 <!DOCTYPE html>
 <html>
     <head>
-        <title>PHP + HTML</title>
+        <title>Counter</title>
+        <!-- CSS -->
+	<meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet"><!-- latest compiled and minified CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"><!-- custom CSS -->
+	<link rel="stylesheet" type="text/css" href="/css/counter.css"> 
     </head>
     <body>
-        <?php echo $message; ?>
+    	<!-- current counter value -->
+    	<h1><?= $counter ?></h1>
+    	<br>
+    	<div class=Aligner>
+    		<!-- Link to click to make counter go up -->
+    		<button><a href="/php/counter.php?counter=<?= $counter + 1; ?>">UP</a></button>
+    	</div>
+    	<div class=Aligner>
+    		<!-- Link to click to make counter go down -->
+    		<button><a href="/php/counter.php?counter=<?= $counter - 1; ?>">DOWN</a></button>
+    	</div>
     </body>
 </html>
